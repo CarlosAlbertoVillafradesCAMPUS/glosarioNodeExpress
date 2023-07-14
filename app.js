4065489f2ef57a0a979a552e609ef1752009df19
CarlosAlbertoVillafradesCAMPUS <cavillafrades@gmail.com>
2023-07-14 18:03:44 +0000

    feat(NODE): :sparkles: Metodo fs.readFile
    El metodo se utilizar para leer ficheros  se le pasan parametros como el path, la configuracion y el callback

--- app.js
+++ app.js
@@ -1,5 +1,14 @@
- //require() sin la extencion .js
+// Require fs
+/*
+fs.readFile(path, options, callback):
+Lee el contenido de un archivo de forma asincrónica 
+ path: Ruta del archivo
+ options: Opciones de lectura, como la codificacion
+ callback: Funcion de devolucion que recibe err(error) y data(contenido del archivo)
+ */
+let fs = require('fs');
 
-const config = require("./module");
-
-console.log(module);
+//Se utiliza el archivo
+fs.readFile("demo.txt", 'utf-8', (err,data)=>{
+    console.log(data);
+})
